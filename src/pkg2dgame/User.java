deeb352e28330953f9b1db5fc8583d49c204b880
Joseph <Joseph@172.17.111.193>
2017-04-28 03:19:55 -0400

    AI is now able to build basic base

--- src/pkg2dgame/User.java
+++ src/pkg2dgame/User.java
@@ -5,9 +5,10 @@
  */
 package pkg2dgame;
 
+import Terrain.Tile;
 import Units.*;
-import Units.GameUnits.BasicFactory;
-import Units.GameUnits.FieldTruck;
+import Units.GameUnits.*;
+import java.util.ArrayList;
 
 /**
  * Used for both plater and AI commands
@@ -17,15 +18,16 @@ import Units.GameUnits.FieldTruck;
 public class User {
 
     //FIELDS
-
     public Team team;           //team player is on
     public int metal, oil;      //amount of resources they have
     public boolean isAI;          //is the player AI controlled?
     public final int playerID;          //unique identifier for the player
     private static int playerIDs = 1;   //this is what determines that unique id. incremenets with each player that is added
     public UI ui;
+    public int playSpeed = 100;          //how often it makes plays. ie every 20 ticks, make a play
+    private int currentTick = 0;        //keeps track of current tick to know when to make a play
+//constructor
 
-    //constructor
     public User(Team team, boolean isAI) {
         this.team = team;
         this.isAI = isAI;
@@ -44,6 +46,9 @@ public class User {
     public void allInAttack(int x, int y) {
         for (Unit u : Game.handler.getUnits()) {
             if (u.team == this.team) {
+                if (u.weapon == Weapon.none) {
+                    continue;   //dont use units with no weapon
+                }
                 u.issueAttackMove(x, y);
             }
         }
@@ -63,19 +68,151 @@ public class User {
     }
 
     /**
+     * runs every game tick
+     */
+    public void tick() {
+        currentTick++;
+        if (currentTick >= playSpeed) {
+            try {
+                makePlay();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            currentTick = 0;
+        }
+    }
+
+    /**
+     * determines what the AI should do based on its situation
+     */
+    public void makePlay() {
+        System.out.println("MAKING PLAY");
+        boolean hasIronMine = false;
+        boolean hasOilRig = false;
+        ArrayList<FieldTruck> trucks = new ArrayList<>();
+        ArrayList<BasicFactory> factorys = new ArrayList<>();
+        int distanceFromClosestPatch = 9999999;
+        Coordinate closestPatch = null;
+        int numMines = 0, numRigs = 0;
+
+        for (Unit u : Game.handler.getUnits()) {
+            if (u.name == "Iron Mine" && u.team == team) {
+                hasIronMine = true;
+                numMines++;
+            } else if (u.name == "Oil Rig" && u.team == team) {
+                hasOilRig = true;
+                numRigs++;
+            }
+        }
+
+        for (Unit u : Game.handler.getUnits()) {
+            if (u.name == "Field Truck" && u.team == team) {
+                FieldTruck ft = (FieldTruck)u;
+               // if(ft.queued)continue;
+                trucks.add(ft);
+            }
+            if (u.name == "Factory" && u.team == team) {
+                factorys.add((BasicFactory) u);
+            }
+        }
+
+        //if we dont have an iron mine, attempt to create an iron mine at a point nearest our field truck or factory
+        if (numMines < 3) {
+            int truckToUse = Main.generateRandom(0, trucks.size());
+            if (trucks.size() > 0) {    //if we have a truck but no mine
+                for (Coordinate c : Game.myMap.coordinates) {
+                    if (trucks.get(truckToUse).pf.getDistanceFrom(c.x * Tile.SIZE, c.y * Tile.SIZE) < distanceFromClosestPatch && c.fertility == 2 && Game.mainGrid.getOccupantOfRange(c.x * Tile.SIZE - 100, c.x * Tile.SIZE + 100, c.y * Tile.SIZE + 100, c.y * Tile.SIZE - 100, trucks.get(truckToUse)) == null) {
+                        closestPatch = c;
+                        distanceFromClosestPatch = trucks.get(truckToUse).pf.getDistanceFrom(c.x * Tile.SIZE, c.y * Tile.SIZE);
+                    }
+                }
+                System.out.println("building at " + closestPatch + "Distance: " + distanceFromClosestPatch);
+                System.out.println("Occupant: " + Game.mainGrid.getObjectAt(closestPatch.x * Tile.SIZE, closestPatch.y * Tile.SIZE));
+                if (closestPatch != null) {
+                    trucks.get(truckToUse).BuildAt(closestPatch.x * Tile.SIZE + Main.generateRandom(0, 25), closestPatch.y * Tile.SIZE + Main.generateRandom(-25, 25), 3);
+                    trucks.remove(trucks.get(truckToUse));
+                }
+
+            } else if (factorys.size() > 0) {
+                distanceFromClosestPatch = 999999;
+                for (Coordinate c : Game.myMap.coordinates) {
+                    if (factorys.get(0).pf.getDistanceFrom(c.x * Tile.SIZE, c.y * Tile.SIZE) < distanceFromClosestPatch && c.fertility == 2) {
+                        closestPatch = c;
+                        distanceFromClosestPatch = factorys.get(0).pf.getDistanceFrom(c.x * Tile.SIZE, c.y * Tile.SIZE);
+                    }
+                }
+                if (closestPatch != null) {
+                   // this.buildIronMine(closestPatch.x*Tile.SIZE, closestPatch.y*Tile.SIZE);
+                    this.buildTruck();
+                }
+            }
+        }
+
+        if (numRigs < 2) {
+            System.out.println(trucks.size() + " trucks");
+            int truckToUse = Main.generateRandom(0, trucks.size());
+            if (trucks.size() > 0) {    //if we have a truck but no mine
+                for (Coordinate c : Game.myMap.coordinates) {
+                    if (trucks.get(truckToUse).pf.getDistanceFrom(c.x * Tile.SIZE, c.y * Tile.SIZE) < distanceFromClosestPatch && c.fertility == 1 && Game.mainGrid.getOccupantOfRange(c.x * Tile.SIZE - 100, c.x * Tile.SIZE + 100, c.y * Tile.SIZE + 100, c.y * Tile.SIZE - 100, trucks.get(truckToUse)) == null) {
+                        System.out.println(Game.mainGrid.getOccupantOfRange(c.x * Tile.SIZE - 50, c.x * Tile.SIZE + 50, c.y * Tile.SIZE - 50, c.y * Tile.SIZE + 50, null));
+                        closestPatch = c;
+                        distanceFromClosestPatch = trucks.get(truckToUse).pf.getDistanceFrom(c.x * Tile.SIZE, c.y * Tile.SIZE);
+                    }
+                }
+                if (closestPatch != null) {
+                    trucks.get(truckToUse).BuildAt(closestPatch.x * Tile.SIZE + Main.generateRandom(0, 25), closestPatch.y * Tile.SIZE + Main.generateRandom(-25, 25), 2);
+                    trucks.remove(trucks.get(truckToUse));
+                }
+
+            } else if (factorys.size() > 0) {
+                distanceFromClosestPatch = 999999;
+                for (Coordinate c : Game.myMap.coordinates) {
+                    if (factorys.get(0).pf.getDistanceFrom(c.x * Tile.SIZE, c.y * Tile.SIZE) < distanceFromClosestPatch && c.fertility == 1) {
+                        closestPatch = c;
+                        distanceFromClosestPatch = factorys.get(0).pf.getDistanceFrom(c.x * Tile.SIZE, c.y * Tile.SIZE);
+                    }
+                }
+                if (closestPatch != null) {
+                   //this.buildOilrig(closestPatch.x*Tile.SIZE, closestPatch.y*Tile.SIZE);
+                    this.buildTruck();
+                }
+            }
+        }
+        if(metal > 500 && factorys.size() < 2 && hasIronMine){
+            this.buildFactory(factorys.get(factorys.size()-1).x + 200, factorys.get(factorys.size()-1).y - 200);    //build up and to the right to help with pathing
+        }
+        
+        if(metal < oil && oil > 100 && metal > 50 && hasIronMine){
+            for(BasicFactory bf: factorys){
+                bf.setDestinaion(bf.x+Main.generateRandom(80, 500), bf.y+Main.generateRandom(80,500));
+                bf.Produce3();//make helicopters if we have too much oil
+            }
+        }
+        
+        if(metal > 150 && hasIronMine && oil > 50){
+           for(BasicFactory bf : factorys) {
+                bf.setDestinaion(bf.x + Main.generateRandom(80, 500), bf.y + Main.generateRandom(80, 500));
+               bf.Produce1(); //make tanks
+           }
+        }
+        System.out.println("ENDING PLAY");
+    }
+
+    /**
      * makes the AI build an iron mine at a specified coordinate if able AI will
      * attempt to build a fieldtruck if none are available to construct the
      * building
+     *
      * @param x
      * @param y
      */
     public void buildIronMine(int x, int y) {
-        boolean truckAvail = false;
         for (Unit u : Game.handler.getUnits()) {
             if (u.name == "Field Truck" && u.team == team) {
-                truckAvail = true;
                 FieldTruck ft = (FieldTruck) u;
-                if(ft.queued)continue;
+                if (ft.queued) {
+                    continue;
+                }
                 ft.BuildAt(x, y, 3);
                 return;
             }
@@ -96,21 +233,22 @@ public class User {
             }
         }
     }
-    
-     /**
+
+    /**
      * makes the AI build an oilrig at a specified coordinate if able AI will
      * attempt to build a fieldtruck if none are available to construct the
      * building
+     *
      * @param x
      * @param y
      */
-    public void buildOilrig(int x, int y){
-        boolean truckAvail = false;
+    public void buildOilrig(int x, int y) {
         for (Unit u : Game.handler.getUnits()) {
             if (u.name == "Field Truck" && u.team == team) {
-                truckAvail = true;
                 FieldTruck ft = (FieldTruck) u;
-                if(ft.queued)continue;
+                if (ft.queued) {
+                    continue;
+                }
                 ft.BuildAt(x, y, 2);
                 return;
             }
@@ -123,7 +261,7 @@ public class User {
                     continue;  //ignore the factory if it is already building somthing
                 }
                 bf.Produce2();
-                if (bf.lastProduced.name == "Field Truck" && bf.lastProduced.isAlive) {
+                if (bf.lastProduced != null && bf.lastProduced.name == "Field Truck" && bf.lastProduced.isAlive) {
                     FieldTruck ft = (FieldTruck) bf.lastProduced; //if we successfully built a field truck, have it go straight to building our mine 
                     ft.BuildAt(x, y, 2);
                 }
@@ -131,21 +269,22 @@ public class User {
             }
         }
     }
-    
+
     /**
      * makes the AI build a factory at a specified coordinate if able AI will
      * attempt to build a fieldtruck if none are available to construct the
      * building
+     *
      * @param x
      * @param y
      */
-    public void buildFactory(int x, int y){
-         boolean truckAvail = false;
+    public void buildFactory(int x, int y) {
         for (Unit u : Game.handler.getUnits()) {
             if (u.name == "Field Truck" && u.team == team) {
-                truckAvail = true;
                 FieldTruck ft = (FieldTruck) u;
-                if(ft.queued)continue;
+                if (ft.queued) {
+                    continue;
+                }
                 ft.BuildAt(x, y, 1);
                 return;
             }
@@ -163,6 +302,20 @@ public class User {
                     ft.BuildAt(x, y, 1);
                 }
                 return;   //return as we only need to build one
+            }
+        }
+    }
+    
+    /**
+     * orders one of the available factories to produce a field truck
+     */
+    public void buildTruck(){
+        for(Unit u : Game.handler.getUnits()){
+            if(u.name == "Factory" && u.team == team){
+                BasicFactory fac = (BasicFactory)u;
+                if(fac.isProducing)continue;
+                else fac.Produce2();
+                return;
             }
         }
     }
